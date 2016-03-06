4350fc1f4a15d7a58e34b80233178b9751fae952
Oliver Sõro <oliver.s6ro@gmail.com>
2016-03-06 20:06:15 +0200

    fp-in-scala: exercises 2.3., 2.4., 2.5. - curry, uncurry, compose.

--- fp-in-scala/src/main/scala/Hof.scala
+++ fp-in-scala/src/main/scala/Hof.scala
@@ -36,4 +36,23 @@ object Hof {
     loop(0)
   }
 
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
+    (b: B) => f(a, b)
+  }
+
+  /** Exercise 2.3. */
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => ((b: B) => f(a, b))
+  }
+
+  /** Exercise 2.4. */
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  /** Exercise 2.5. */
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+
 }
