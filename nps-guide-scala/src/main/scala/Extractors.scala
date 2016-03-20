ac3e440a950b77b92cd20c62640b8db6a4cc491c
Oliver Sõro <oliver.s6ro@gmail.com>
2016-03-20 18:31:07 +0200

    nps-guide-scala: extractors with multiple arguments.

--- nps-guide-scala/src/main/scala/Extractors.scala
+++ nps-guide-scala/src/main/scala/Extractors.scala
@@ -1,14 +1,17 @@
 trait User {
   def name: String
+  def score: Int
 }
 
-class FreeUser(val name: String) extends User
-class PremiumUser(val name: String) extends User
+class FreeUser(val name: String, val score: Int, val upgradeProbability: Double) extends User
+class PremiumUser(val name: String, val score: Int) extends User
 
 object FreeUser {
-  def unapply(user: FreeUser): Option[String] = Some(user.name)
+  def unapply(user: FreeUser): Option[(String, Int, Double)] =
+    Some(user.name, user.score, user.upgradeProbability)
 }
 
 object PremiumUser {
-  def unapply(user: PremiumUser): Option[String] = Some(user.name)
+  def unapply(user: PremiumUser): Option[(String, Int)] =
+    Some(user.name, user.score)
 }
--- nps-guide-scala/src/test/scala/ExtractorsTest.scala
+++ nps-guide-scala/src/test/scala/ExtractorsTest.scala
@@ -4,12 +4,13 @@ class ExtractorsTest extends Specification {
 
   "Extractors#PremiumUser" should {
     "provide name in an extractor pattern" in {
-      val u = new PremiumUser("King")
-      val actualName = u match {
-        case FreeUser(name) => name
-        case PremiumUser(name) => name
+      val u = new FreeUser("King", 3000, 0.75)
+      val (actualName, score) = u match {
+        case FreeUser(name, s, _) => (name, s)
+        case PremiumUser(name, s) => (name, s)
       }
       actualName must beEqualTo("King")
+      score must beEqualTo(3000)
     }
   }
 
