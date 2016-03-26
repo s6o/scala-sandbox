88936907ef6d92534d7d00d453a63cca31c8e442
Oliver Sõro <oliver.s6ro@gmail.com>
2016-03-26 20:17:12 +0200

    nps-guide-scala: Try[A] in for comprehensions.

--- nps-guide-scala/src/main/scala/ErrorHandling.scala
+++ nps-guide-scala/src/main/scala/ErrorHandling.scala
@@ -1,4 +1,6 @@
+import scala.io.Source
 import scala.util.Try
+import java.io.InputStream
 import java.net.URL
 
 case class Customer(age: Int)
@@ -25,7 +27,27 @@ object ErrorHandling {
 
   def parseUrl(url: String): Try[URL] = Try(new URL(url))
 
-  def stdinUrl(): Try[URL] =
-    parseUrl(scala.io.StdIn.readLine()).getOrElse[Try[URL]](parseUrl("http://duckduck.go"))
+  def stdinUrl(): Try[URL]  =
+    Try(parseUrl(scala.io.StdIn.readLine()).getOrElse(new URL("http://duckduck.go")))
 
+  def urlInputStream(url: String): Try[InputStream] = parseUrl(url).flatMap { u =>
+    Try(u.openConnection()).flatMap(conn => Try(conn.getInputStream))
+  }
+
+  def parseHttpUrl(url: String): Try[URL] = parseUrl(url).filter(_.getProtocol == "http")
+
+  def getUrlContentBook(url: String): Try[Iterator[String]] =
+    for {
+      url <- parseUrl(url)
+      connection <- Try(url.openConnection())
+      is <- Try(connection.getInputStream)
+      source = Source.fromInputStream(is)
+    } yield source.getLines()
+
+  // How could the input stream be closed properly?
+  def getUrlContents(url: String): Try[Iterator[String]] =
+    for {
+      is <- urlInputStream(url)
+      source = Source.fromInputStream(is)
+    } yield source.getLines()
 }
