35ae1c22978776f065c799c000a850a15447ed8f
Oliver Sõro <oliver.s6ro@gmail.com>
2016-04-16 15:19:05 +0300

    nps-guide-scala: deliverying on a promise, does not work as expected.
    For some reason the Future's onComplete Success case never fires. ATM
    cannot figure out why?!
    
    [master] scala-sandbox/nps_guide_scala: runMain Politics
    [info] Running Politics
    [error] Picked up _JAVA_OPTIONS: -Xms1024m -Xmx2G -Xss256m
    [info] Let's see if they remember their promise...
    [info] Starting the new legislative period.
    [info] We reduced the taxes! You must reelect us!
    [success] Total time: 2 s, completed Apr 16, 2016 3:14:18 PM
    
    Perhaps it has to do something with the error?!
    
    Missing and additional print message from onComplete (respective snippet):
    
        case Success(TaxCut(reduction)) =>
                println(s"Yay! They cut our taxes by $reduction percentage points!")

--- nps-guide-scala/src/main/scala/Politics.scala
+++ nps-guide-scala/src/main/scala/Politics.scala
@@ -1,19 +1,43 @@
-import concurrent.Future
-import concurrent.Promise
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 case class TaxCut(reduction: Int)
 
+object Goverment {
+
+  def redeemCampaignPledge(): Future[TaxCut] = {
+    val p = Promise[TaxCut]()
+    // This here is to move the computation to another thread i.e. not the caller thread
+    // and return immediately the Future object from the Promise p. The caller will receive
+    // an answer after ~ 2 seconds
+    Future {
+      println("Starting the new legislative period.")
+      Thread.sleep(2000)
+      p.success(TaxCut(20))
+      println("We reduced the taxes! You must reelect us!")
+    }
+    p.future
+  }
+
+}
+
 object Politics {
 
-  // Promise creation example 1
-  val taxcut1 = Promise[TaxCut]()
+  def main(args: Array[String]): Unit = {
+    val tf: Future[TaxCut] = Goverment.redeemCampaignPledge()
 
-  // Promise creation example 2
-  val taxcut2: Promise[TaxCut] = Promise()
+    println("Let's see if they remember their promise...")
 
-  val taxcutF1: Future[TaxCut] = taxcut1.future
+    tf.onComplete {
+      case Success(TaxCut(reduction)) =>
+        println(s"Yay! They cut our taxes by $reduction percentage points!")
+      case Failure(ex) =>
+        println(s"They broke their promises! Because of a ${ex.getMessage}")
+    }
 
-  // to successfully deliver on a Promise
-  taxcut1.success(TaxCut(20))
+    Await.result(tf, 10.seconds)
+  }
 
 }
