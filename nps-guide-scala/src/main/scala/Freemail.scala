1bc0a38531f554861db1ab8bf4a25ba7a2b0e83a
Oliver Sõro <oliver.s6ro@gmail.com>
2016-04-20 08:45:28 +0300

    nps-guide-scala: DRY continued - composing predicates.

--- nps-guide-scala/src/main/scala/Freemail.scala
+++ nps-guide-scala/src/main/scala/Freemail.scala
@@ -8,6 +8,10 @@ object Freemail {
   def newMailsForUser(mails: Seq[Email], f: EmailFilter): Seq[Email] = mails.filter(f)
   def complement[A](predicate: A => Boolean): (A => Boolean) = (a: A) => !predicate(a)
 
+  def any[A](predicates: (A => Boolean)*): A => Boolean = (a: A) => predicates.exists(p => p(a))
+  def none[A](predicates: (A => Boolean)*): A => Boolean = complement(any(predicates: _*))
+  def every[A](predicates: (A => Boolean)*): A => Boolean = none(predicates.view.map(complement(_)): _*)
+
   val sentByOneOf: Set[String] => EmailFilter = senders => email => senders.contains(email.sender)
   val notSentByAnyOf: Set[String] => EmailFilter = sentByOneOf.andThen(g => complement(g))
   val sizeConstraint: SizeChecker => EmailFilter = f => email => f(email.text.length)
@@ -22,14 +26,18 @@ object Freemail {
 
   def main(args: Array[String]): Unit = {
 
-    val emailFilter: EmailFilter = notSentByAnyOf(Set("johndoe@example.com"))
+    val filter: EmailFilter = every(
+      notSentByAnyOf(Set("johndoe@example.com")),
+      minimumSize(100),
+      maximumSize(10000)
+    )
     val mails = Email(
       subject = "It's me again, your stalker friend!",
       text = "Hello my friend! How are you?",
       sender = "johndoe@example.com",
       recipient = "me@example.com") :: Nil
 
-    val result = newMailsForUser(mails, emailFilter)
+    val result = newMailsForUser(mails, filter)
 
     println(result)
   }
