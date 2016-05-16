b73c0a3a81dfef496e9a534842dcee96ce9f5c68
Oliver Sõro <oliver.s6ro@gmail.com>
2016-05-16 09:36:01 +0300

    nps-guide-scala: partially applied functions, Freemail continued.

--- nps-guide-scala/src/main/scala/Freemail.scala
+++ nps-guide-scala/src/main/scala/Freemail.scala
@@ -4,6 +4,7 @@ object Freemail {
 
   type EmailFilter = Email => Boolean
   type SizeChecker = Int => Boolean
+  type IntPairPred = (Int, Int) => Boolean
 
   def newMailsForUser(mails: Seq[Email], f: EmailFilter): Seq[Email] = mails.filter(f)
   def complement[A](predicate: A => Boolean): (A => Boolean) = (a: A) => !predicate(a)
@@ -12,11 +13,24 @@ object Freemail {
   def none[A](predicates: (A => Boolean)*): A => Boolean = complement(any(predicates: _*))
   def every[A](predicates: (A => Boolean)*): A => Boolean = none(predicates.view.map(complement(_)): _*)
 
+  def sizeConstraint(pred: IntPairPred, n: Int, email: Email): Boolean = pred(email.text.length, n)
+
+  val gt: IntPairPred = _ > _
+  val ge: IntPairPred = _ >= _
+  val lt: IntPairPred = _ < _
+  val le: IntPairPred = _ <= _
+  val eq: IntPairPred = _ == _
+
+  val minimumSize: (Int, Email) => Boolean = sizeConstraint(ge, _: Int, _: Email)
+  val maximumSize: (Int, Email) => Boolean = sizeConstraint(le, _: Int, _: Email)
+
+  val constr20: (IntPairPred, Email) => Boolean = sizeConstraint(_: IntPairPred, 20, _: Email)
+  val constr30: (IntPairPred, Email) => Boolean = sizeConstraint(_: IntPairPred, 30, _: Email)
+  val min20: EmailFilter = minimumSize(20, _: Email)
+  val max20: EmailFilter = maximumSize(20, _: Email)
+
   val sentByOneOf: Set[String] => EmailFilter = senders => email => senders.contains(email.sender)
   val notSentByAnyOf: Set[String] => EmailFilter = sentByOneOf.andThen(g => complement(g))
-  val sizeConstraint: SizeChecker => EmailFilter = f => email => f(email.text.length)
-  val minimumSize: Int => EmailFilter = n => sizeConstraint(_ >= n)
-  val maximumSize: Int => EmailFilter = n => sizeConstraint(_ >= n)
 
   val addMissingSubject: Email => Email =
     (email: Email) => if (email.subject.isEmpty) email.copy(subject = "No subject") else email
