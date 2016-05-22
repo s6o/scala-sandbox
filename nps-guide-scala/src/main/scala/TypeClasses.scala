52a0978c7fbe6c51d45076d02f37f0cc16ddfc42
Oliver Sõro <oliver.s6ro@gmail.com>
2016-05-22 13:46:17 +0300

    nps-guide-scala: type classes, typical adaptar like approach for example Statistics library.

--- nps-guide-scala/src/main/scala/TypeClasses.scala
+++ nps-guide-scala/src/main/scala/TypeClasses.scala
@@ -1,13 +1,36 @@
-
 object Statistics {
-  def median(xs: Vector[Double]): Double = xs(xs.size / 2)
-
-  def quartiles(xs: Vector[Double]): (Double, Double, Double) =
-    (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
-
-  def iqr(xs: Vector[Double]): Double = quartiles(xs) match {
-    case (lowerQuartile, _, upperQuartile) => upperQuartile - lowerQuartile
+  trait NumberLike[A] {
+    def get: A
+    def plus(y: NumberLike[A]): NumberLike[A]
+    def minus(y: NumberLike[A]): NumberLike[A]
+    def divide(y: Int): NumberLike[A]
   }
 
-  def mean(xs: Vector[Double]): Double = xs.reduce(_ + _) / xs.size
+  case class NumberLikeDouble(x: Double) extends NumberLike[Double] {
+    def get: Double = x
+    def minus(y: NumberLike[Double]): NumberLike[Double] = NumberLikeDouble(x - y.get)
+    def plus(y: NumberLike[Double]): NumberLike[Double] = NumberLikeDouble(x + y.get)
+    def divide(y: Int): NumberLike[Double] = NumberLikeDouble(x / y)
+  }
+
+  case class NumberLikeInt(x: Int) extends NumberLike[Int] {
+    def get: Int = x
+    def minus(y: NumberLike[Int]): NumberLike[Int] = NumberLikeInt(x - y.get)
+    def plus(y: NumberLike[Int]): NumberLike[Int] = NumberLikeInt(x + y.get)
+    def divide(y: Int): NumberLike[Int] = NumberLikeInt(x / y)
+  }
+
+  type Quartile[A] = (NumberLike[A], NumberLike[A], NumberLike[A])
+
+  def median[A](xs: Vector[NumberLike[A]]): NumberLike[A] = xs(xs.size / 2)
+
+  def quartiles[A](xs: Vector[NumberLike[A]]): Quartile[A] =
+    (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+
+  def iqr[A](xs: Vector[NumberLike[A]]): NumberLike[A] = quartiles(xs) match {
+    case (lowerQuartile, _, upperQuartile) => upperQuartile.minus(lowerQuartile)
+  }
+
+  def mean[A](xs: Vector[NumberLike[A]]): NumberLike[A] =
+    xs.reduce(_.plus(_)).divide(xs.size)
 }
