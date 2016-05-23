2b4c0a01f0d03d4bd5771ce82988a6dd6d28c303
Oliver Sõro <oliver.s6ro@gmail.com>
2016-05-23 10:12:51 +0300

    nps-guide-scala: type classes, Statistics implementation with NumerLike type class.
    The main (body) did not work exactly as referenced in the book. Had to add
    
      import NumerLike
    
    to the main function/method body for implicit NumberLike implementations to
    be found by the compiler.

--- nps-guide-scala/src/main/scala/TypeClasses.scala
+++ nps-guide-scala/src/main/scala/TypeClasses.scala
@@ -1,36 +1,44 @@
+object Math {
+  import annotation.implicitNotFound
+  @implicitNotFound("No member of type class NumberLike in scope for ${T}")
+  trait NumberLike[T] {
+    def plus(x: T, y: T): T
+    def divide(x: T, y: Int): T
+    def minus(x: T, y: T): T
+  }
+}
+
+object NumberLike {
+  implicit object NumberLikeDouble extends Math.NumberLike[Double] {
+    def plus(x: Double, y: Double): Double = x + y
+    def divide(x: Double, y: Int): Double = x / y
+    def minus(x: Double, y: Double): Double = x - y
+  }
+
+  implicit object NumberLikeInt extends Math.NumberLike[Int] {
+    def plus(x: Int, y: Int): Int = x + y
+    def divide(x: Int, y: Int): Int = x / y
+    def minus(x: Int, y: Int): Int = x - y
+  }
+}
+
 object Statistics {
-  trait NumberLike[A] {
-    def get: A
-    def plus(y: NumberLike[A]): NumberLike[A]
-    def minus(y: NumberLike[A]): NumberLike[A]
-    def divide(y: Int): NumberLike[A]
+  import Math.NumberLike
+
+  def mean[T](xs: Vector[T])(implicit ev: NumberLike[T]): T = ev.divide(xs.reduce(ev.plus), xs.size)
+
+  def median[T](xs: Vector[T])(implicit ev: NumberLike[T]): T = xs(xs.size / 2)
+
+  def quartiles[T](xs: Vector[T])(implicit ev: NumberLike[T]): (T, T, T) =
+    (xs(xs.size / 4), median[T](xs), xs(xs.size / 4 * 3))
+
+  def iqr[T](xs: Vector[T])(implicit ev: NumberLike[T]): T = quartiles(xs) match {
+    case (lowerQuartile, _, upperQuartile) => ev.minus(upperQuartile, lowerQuartile)
   }
 
-  case class NumberLikeDouble(x: Double) extends NumberLike[Double] {
-    def get: Double = x
-    def minus(y: NumberLike[Double]): NumberLike[Double] = NumberLikeDouble(x - y.get)
-    def plus(y: NumberLike[Double]): NumberLike[Double] = NumberLikeDouble(x + y.get)
-    def divide(y: Int): NumberLike[Double] = NumberLikeDouble(x / y)
+  def main(args: Array[String]): Unit = {
+    import NumberLike._
+    val numbers = Vector[Double](13, 23.0, 42, 45, 61, 73, 96, 100, 199, 420, 900, 3839)
+    println(Statistics.mean(numbers))
   }
-
-  case class NumberLikeInt(x: Int) extends NumberLike[Int] {
-    def get: Int = x
-    def minus(y: NumberLike[Int]): NumberLike[Int] = NumberLikeInt(x - y.get)
-    def plus(y: NumberLike[Int]): NumberLike[Int] = NumberLikeInt(x + y.get)
-    def divide(y: Int): NumberLike[Int] = NumberLikeInt(x / y)
-  }
-
-  type Quartile[A] = (NumberLike[A], NumberLike[A], NumberLike[A])
-
-  def median[A](xs: Vector[NumberLike[A]]): NumberLike[A] = xs(xs.size / 2)
-
-  def quartiles[A](xs: Vector[NumberLike[A]]): Quartile[A] =
-    (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
-
-  def iqr[A](xs: Vector[NumberLike[A]]): NumberLike[A] = quartiles(xs) match {
-    case (lowerQuartile, _, upperQuartile) => upperQuartile.minus(lowerQuartile)
-  }
-
-  def mean[A](xs: Vector[NumberLike[A]]): NumberLike[A] =
-    xs.reduce(_.plus(_)).divide(xs.size)
 }
