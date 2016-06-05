b9fa4803477b3b2dae3f2ca91e58f71164ab6e3b
Oliver Sõro <oliver.s6ro@gmail.com>
2016-06-05 07:00:22 +0300

    nps-guide-scala: Actors - answering to fire and forget messages via sender.
    def !(message: Any)(implicit sender: ActorRef = Actor.noSender): Unit
    
    Had to re-name the Customer actor to CafeCustomer as Customer exists already
    in another nps-guide-scala example.

--- nps-guide-scala/src/main/scala/CoffeeHouse.scala
+++ nps-guide-scala/src/main/scala/CoffeeHouse.scala
@@ -1,6 +1,10 @@
 import akka.actor.{Actor, ActorSystem}
 import akka.actor.{ActorRef, Props}
 
+case class Bill(cents: Int)
+case object ClosingTime
+case object CaffeineWithdrawalWarning
+
 sealed trait CoffeeRequest
 case object CappuccinoRequest extends CoffeeRequest
 case object EspressoRequest extends CoffeeRequest
@@ -8,17 +12,30 @@ case object EspressoRequest extends CoffeeRequest
 class Barista extends Actor {
   def receive: PartialFunction[Any, Unit] = {
     case CappuccinoRequest =>
+      sender ! Bill(250)
       println("I have to prepare a cappuccino")
     case EspressoRequest =>
+      sender ! Bill(200)
       println("Let's prepare an espresso")
+    case ClosingTime =>
+      context.system.shutdown()
+  }
+}
+
+class CafeCustomer(caffeineSource: ActorRef) extends Actor {
+  def receive: PartialFunction[Any, Unit] = {
+    case CaffeineWithdrawalWarning =>
+      caffeineSource ! EspressoRequest
+    case Bill(cents) =>
+      println(s"I have to pay $cents cents, or else!")
   }
 }
 
 object CoffeeHouse extends App {
   val system = ActorSystem("CoffeeHouse")
   val barista: ActorRef = system.actorOf(Props[Barista], "Barista")
-  barista ! CappuccinoRequest
-  barista ! EspressoRequest
-  println("I ordered a cappuccino and an espresso")
-  system.shutdown()
+  val customer: ActorRef = system.actorOf(Props(classOf[CafeCustomer], barista), "Customer")
+
+  customer ! CaffeineWithdrawalWarning
+  barista ! ClosingTime
 }
